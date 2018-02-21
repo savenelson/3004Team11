45fd9a586b3ef8255e618368d6ccfa912558da76
Zac <zacharystroudtaylor@cmail.carleton.ca>
2018-02-21 13:18:42 -0500

    passing state

--- src/main/java/core/Control.java
+++ src/main/java/core/Control.java
@@ -1,6 +1,9 @@
 package core;
 
+import java.util.concurrent.TimeUnit;
+
 import javafx.application.Application;
+import javafx.scene.control.Label;
 
 public class Control{
 
@@ -9,31 +12,28 @@ public class Control{
 	
 	private static String testString;
 	
-	// args[0]: number of players
-	public static void main(String args []){
 	
-		Control control = new Control();
+	public Control(View view) {
 		
-		testString = "I'm the private test string";
+		this.view = view;
 		
-		gameInit(args, control);
+		gameInit(null);
 		
 		mainLoop();
 		
-		Application.launch(View.class, args);
-		
-		////TEST
+		//TEST
 		
 		//model.CardsTest();
 		
-		////END TEST
+		//END TEST
 	}
 	
-	public static void gameInit(String args [], Control control){
+
+	public void gameInit(String args []){
 		
 		int numPlayers;
 		
-		if(args.length == 0){
+		if(args == null || args.length == 0){
 			numPlayers = 4;
 		}
 		else{
@@ -41,9 +41,8 @@ public class Control{
 		}
 		if(numPlayers >= 2 && numPlayers <= 4)
 		{
-			//View view = new View(null);
 
-			model = new Model(control);
+			model = new Model(this);
 		}
 		else{// Maybe make a view constructor that displays invalid number of players type thing...??????????????
 			 // POP UP MESSAGE????? from control?
@@ -57,38 +56,23 @@ public class Control{
 		model.deal();
 	}
 
-	public static void mainLoop(){
+	public void mainLoop(){
 		
 		boolean win = false;
 		
-		StoryCardAndCurrentPlayer curr = new StoryCardAndCurrentPlayer();
 		
 		while(!win){
 			
-			curr = model.drawStoryCard(curr);
-			
-			System.out.println("Current player: " + curr.currentPlayer.toString() + ", Current story card: " + curr.currentStoryCard.toString());
-			
 			win = !win;
 			
-//			int i;
-//			for(i = 0; i < model.getPlayers().length; ++i){
-//				if(i == 2){
-//					win = true;
-//					break;
-//				}
-//				System.out.println(i);
-//			}
-			
+
 		}
 	}
 	
-	public static class StoryCardAndCurrentPlayer{
-		Player currentPlayer;	
-		Card currentStoryCard;
+	public State getState(){
+		State state = model.getState();
+		return state;
 	}
-	
-
 	
 	public void printTestString(){System.out.println(testString);}
 
