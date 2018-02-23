d875af2c72e9998d760d2886a4da6dacce17eb3d
Sammy Diamantstein <lev18d@users.noreply.github.com>
2018-02-23 18:24:03 -0500

    Revert "Sammy3"

--- src/test/java/core/CardInstantiationTest.java
+++ src/test/java/core/CardInstantiationTest.java
@@ -8,7 +8,7 @@ public class CardInstantiationTest {
 
 	@Test
 	public void test() {
-
+		
 		// WEAPONS
 		WeaponCard sword = new WeaponCard(WeaponCard.SWORD_NAME, WeaponCard.SWORD_BATTLE_POINTS);
 		assertEquals("Sword", sword.getName());
@@ -34,7 +34,7 @@ public class CardInstantiationTest {
 		assertEquals("Excalibur", excalibur.getName());
 		assertEquals(30, excalibur.getBattlePoints());
 
-
+		
 		// FOES
 		FoeCard thieves = new FoeCard(FoeCard.THIEVES_NAME, FoeCard.THIEVES_BATTLE_POINTS, FoeCard.THIEVES_ALT_BATTLE_POINTS, new NoSpecial());
 		assertEquals("Thieves", thieves.getName());
@@ -45,12 +45,12 @@ public class CardInstantiationTest {
 		assertEquals("Boar", boar.getName());
 		assertEquals(5, boar.getBattlePoints());
 		assertEquals(15, boar.getAltBattlePoints());
-
+		
 		FoeCard saxons = new FoeCard(FoeCard.SAXONS_NAME, FoeCard.SAXONS_BATTLE_POINTS, FoeCard.SAXONS_ALT_BATTLE_POINTS, new NoSpecial());
 		assertEquals("Saxons", saxons.getName());
 		assertEquals(10, saxons.getBattlePoints());
 		assertEquals(20, saxons.getAltBattlePoints());
-
+		
 		FoeCard robberKnight = new FoeCard(FoeCard.ROBBER_KNIGHT_NAME, FoeCard.ROBBER_KNIGHT_BATTLE_POINTS, FoeCard.ROBBER_KNIGHT_ALT_BATTLE_POINTS, new NoSpecial());
 		assertEquals("RobberKnight", robberKnight.getName());
 		assertEquals(15, robberKnight.getBattlePoints());
@@ -91,18 +91,13 @@ public class CardInstantiationTest {
 		assertEquals(50, dragon.getBattlePoints());
 		assertEquals(70, dragon.getAltBattlePoints());
 
-
-
+	
+	
 		//INSTRUCTIONS:
-		// give cards variable name ie AllyCard sirGalahad = new AllyCard(...);
-		// assert all relevant attributes (add getters to Class if necessary)
-		// Do this for all allies, amours,
-
-//Story Deck --> Event - Quest -Tournament
-
-
-//Tournament Test 3 2 1 0
-
+					// give cards variable name ie AllyCard sirGalahad = new AllyCard(...);
+					// assert all relevant attributes (add getters to Class if necessary)
+					// Do this for all allies, amours, story deck and rank cards
+		
 		// ALLIES
 		new AllyCard(AllyCard.SIR_GALAHAD_NAME, AllyCard.SIR_GALAHAD_BATTLE_POINTS, new NoSpecial());
 		new AllyCard(AllyCard.SIR_LANCELOT_NAME, AllyCard.SIR_LANCELOT_BATTLE_POINTS, new NoSpecial());
@@ -114,152 +109,11 @@ public class CardInstantiationTest {
 		new AllyCard(AllyCard.QUEEN_GUINEVERE_NAME, AllyCard.QUEEN_GUINEVERE_BATTLE_POINTS, new BidSpecial(AllyCard.QUEEN_GUINEVERE_BIDS));
 		new AllyCard(AllyCard.QUEEN_ISEULT_NAME, AllyCard.QUEEN_ISEULT_BATTLE_POINTS, new BidSpecial(AllyCard.QUEEN_ISEULT_BIDS));
 		new AllyCard(AllyCard.MERLIN_NAME, AllyCard.MERLIN_BATTLE_POINTS, new NoSpecial());
-
-		TournamentCard camelot = new TournamentCard(TournamentCard.CAMELOT_NAME,TournamentCard.CAMELOT_SHIELDS);
-		assertEquals("AtCamelot", camelot.getName());
-		assertEquals(3, camelot.getNumShields());
-
-		TournamentCard orkney = new TournamentCard(TournamentCard.ORKNEY_NAME,TournamentCard.ORKNEY_SHIELDS);
-		assertEquals("AtOrkney", orkney.getName());
-		assertEquals(2, orkney.getNumShields());
-
-		TournamentCard tintagel = new TournamentCard(TournamentCard.TINTAGEL_NAME,TournamentCard.TINTAGEL_SHIELDS);
-		assertEquals("AtTintagel", tintagel.getName());
-		assertEquals(1, tintagel.getNumShields());
-
-		TournamentCard york = new TournamentCard(TournamentCard.YORK_NAME,TournamentCard.YORK_SHIELDS);
-		assertEquals("AtYork", york.getName());
-		assertEquals(0, york.getNumShields());
-
-
-QuestCard holygrail = new QuestCard(QuestCard.HOLY_GRAIL,	QuestCard.HOLY_GRAIL_STAGES,	QuestCard.HOLY_GRAIL_FOE);
-assertEquals("SearchForTheHolyGrail", holygrail.getName());
-assertEquals(5, holygrail.getNumStages());
-assertEquals("All", holygrail.getFoe());
-
-
-QuestCard greenknight = new QuestCard(QuestCard.GREEN_KNIGHT,	QuestCard.GREEN_KNIGHT_STAGES,	QuestCard.GREEN_KNIGHT_FOE);
-assertEquals("TestOfTheGreenKnight", greenknight.getName());
-assertEquals(4, greenknight.getNumStages());
-assertEquals("Green Knight", greenknight.getFoe());
-
-QuestCard qbeast = new QuestCard(QuestCard.QUESTING_BEAST,	QuestCard.QUESTING_BEAST_STAGES,	QuestCard.QUESTING_BEAST_FOE);
-assertEquals("SearchForTheQuestingBeast", qbeast.getName());
-assertEquals(4, qbeast.getNumStages());
-assertEquals("None", qbeast.getFoe());
-
-QuestCard qhonor = new QuestCard(QuestCard.QUEENS_HONOR,	QuestCard.QUEENS_HONOR_STAGES,	QuestCard.QUEENS_HONOR_FOE);
-assertEquals("DefendTheQueensHonor", qhonor.getName());
-assertEquals(4, qhonor.getNumStages());
-assertEquals("All", qhonor.getFoe());
-
-QuestCard maiden = new QuestCard(QuestCard.FAIR_MAIDEN,	QuestCard.FAIR_MAIDEN_STAGES,	QuestCard.FAIR_MAIDEN_FOE);
-assertEquals("RescueTheFairMaiden", maiden.getName());
-assertEquals(3, maiden.getNumStages());
-assertEquals("Black Knight", maiden.getFoe());
-
-QuestCard enchanted = new QuestCard(QuestCard.ENCHANTED_FORST,	QuestCard.ENCHANTED_FORST_STAGES,	QuestCard.ENCHANTED_FORST_FOE);
-assertEquals("JourneyThroughTheEnchantedForest", enchanted.getName());
-assertEquals(3, enchanted.getNumStages());
-assertEquals("Evil Knight", enchanted.getFoe());
-
-QuestCard karthur = new QuestCard(QuestCard.ARTHURS_ENEMIES,	QuestCard.ARTHURS_ENEMIES_STAGES,	QuestCard.ARTHURS_ENEMIES_FOE);
-assertEquals("VanquishKingArthursEnemies", karthur.getName());
-assertEquals(3, karthur.getNumStages());
-assertEquals("None", karthur.getFoe());
-
-QuestCard slaygragon = new QuestCard(QuestCard.SLAY_DRAGON,	QuestCard.SLAY_DRAGON_STAGES,	QuestCard.SLAY_DRAGON_FOE);
-assertEquals("SlayTheDragon", slaygragon.getName());
-assertEquals(3, slaygragon.getNumStages());
-assertEquals("Dragon", slaygragon.getFoe());
-
-QuestCard boarhunt = new QuestCard(QuestCard.BOAR_HUNT,	QuestCard.BOAR_HUNT_STAGES,	QuestCard.BOAR_HUNT_FOE);
-assertEquals("BoarHunt", boarhunt.getName());
-assertEquals(2, boarhunt.getNumStages());
-assertEquals("Boar", boarhunt.getFoe());
-
-QuestCard saxonraiders = new QuestCard(QuestCard.SAXON_RAIDERS,	QuestCard.SAXON_RAIDERS_STAGES,	QuestCard.SAXON_RAIDERS_FOE);
-assertEquals("RepelTheSaxonRaiders", saxonraiders.getName());
-assertEquals(2, saxonraiders.getNumStages());
-assertEquals("All Saxons", saxonraiders.getFoe());
-
-
-//Rank Card
-/*
-RankCard knight = new RankCard(RankCard.KNIGHT);
-// RankCard knight = new RankCard("Knight");
-assertEquals(10,knight.getBattlePoints());
-
-RankCard squire = new RankCard(RankCard.SQUIRE);
-//RankCard squire = new RankCard("Squire");
-assertEquals(5,squire.getBattlePoints());
-
-
-RankCard championknight = new RankCard(RankCard.CHAMPION_KNIGHT);
-//RankCard championknight = new RankCard("ChampionKnight");
-assertEquals(20,championknight.getBattlePoints());
-
-
-*/
-
-// ALLIES
-//new AllyCard(AllyCard.SIR_GALAHAD_NAME, AllyCard.SIR_GALAHAD_BATTLE_POINTS, new NoSpecial());
-AllyCard galahad = new AllyCard(AllyCard.SIR_GALAHAD_NAME, AllyCard.SIR_GALAHAD_BATTLE_POINTS, new NoSpecial());
-assertEquals("SirGalahad", galahad.getName());
-assertEquals(15, galahad.getBattlePoints());
-
-//	new AllyCard(AllyCard.SIR_LANCELOT_NAME, AllyCard.SIR_LANCELOT_BATTLE_POINTS, new NoSpecial());
-AllyCard lancelot = new AllyCard (AllyCard.SIR_LANCELOT_NAME, AllyCard.SIR_LANCELOT_BATTLE_POINTS, new NoSpecial());
-assertEquals("SirLancelot", lancelot.getName());
-assertEquals(15, lancelot.getBattlePoints());
-
-//	 new AllyCard(AllyCard.KING_ARTHUR_NAME, AllyCard.KING_ARTHUR_BATTLE_POINTS, new BidSpecial(AllyCard.KING_ARTHUR_BIDS));
-AllyCard arthur = new AllyCard(AllyCard.KING_ARTHUR_NAME, AllyCard.KING_ARTHUR_BATTLE_POINTS, new BidSpecial(AllyCard.KING_ARTHUR_BIDS));
- assertEquals("KingArthur", arthur.getName());
- assertEquals(10, arthur.getBattlePoints());
-
-
-
-//	 new AllyCard(AllyCard.SIR_TRISTAN_NAME, AllyCard.SIR_TRISTAN_BATTLE_POINTS, new NoSpecial());
-AllyCard tristan = new AllyCard(AllyCard.SIR_TRISTAN_NAME, AllyCard.SIR_TRISTAN_BATTLE_POINTS,new NoSpecial());
- assertEquals("SirTristan", tristan.getName());
- assertEquals(10, tristan.getBattlePoints());
-
-//	new AllyCard(AllyCard.SIR_PELLINORE_NAME, AllyCard.SIR_PELLINORE_BATTLE_POINTS, new NoSpecial());// SOMETIMES SPECIAL and name is different
-AllyCard pellinore = new AllyCard(AllyCard.KING_PELLINORE_NAME, AllyCard.KING_PELLINORE_BATTLE_POINTS, new NoSpecial());// SOMETIMES SPECIAL and name is different
- assertEquals("KingPellinore", pellinore.getName());
- assertEquals(10, pellinore.getBattlePoints());
-
-//	new AllyCard(AllyCard.SIR_GAWAIN_NAME, AllyCard.SIR_GAWAIN_BATTLE_POINTS, new NoSpecial());
- AllyCard gawain = new AllyCard(AllyCard.SIR_GAWAIN_NAME, AllyCard.SIR_GAWAIN_BATTLE_POINTS, new NoSpecial());
- assertEquals("SirGawain", gawain.getName());
- assertEquals(10, gawain.getBattlePoints());
-
-
-//	new AllyCard(AllyCard.SIR_PERCIVAL_NAME, AllyCard.SIR_PERCIVAL_BATTLE_POINTS, new NoSpecial());
- AllyCard percival = new AllyCard(AllyCard.SIR_PERCIVAL_NAME, AllyCard.SIR_PERCIVAL_BATTLE_POINTS, new NoSpecial());
- assertEquals("SirPercival", percival.getName());
- assertEquals(5, percival.getBattlePoints());
-
-//	new AllyCard(AllyCard.QUEEN_GUINEVERE_NAME, AllyCard.QUEEN_GUINEVERE_BATTLE_POINTS, new BidSpecial(AllyCard.QUEEN_GUINEVERE_BIDS));
- AllyCard guinevere = new AllyCard(AllyCard.QUEEN_GUINEVERE_NAME, AllyCard.QUEEN_GUINEVERE_BATTLE_POINTS, new BidSpecial(AllyCard.QUEEN_GUINEVERE_BIDS));
- assertEquals("QueenGuinevere", guinevere.getName());
- assertEquals(0, guinevere.getBattlePoints());
-
-
-//new AllyCard(AllyCard.QUEEN_ISEULT_NAME, AllyCard.QUEEN_ISEULT_BATTLE_POINTS, new BidSpecial(AllyCard.QUEEN_ISEULT_BIDS));
- AllyCard iseult = new AllyCard(AllyCard.QUEEN_ISEULT_NAME, AllyCard.QUEEN_ISEULT_BATTLE_POINTS, new BidSpecial(AllyCard.QUEEN_ISEULT_BIDS));
- assertEquals("QueenIseult", iseult.getName());
- assertEquals(0, iseult.getBattlePoints());
-
-
-// new AllyCard(AllyCard.MERLIN_NAME, AllyCard.MERLIN_BATTLE_POINTS, new NoSpecial());
- AllyCard merlin =  new AllyCard(AllyCard.MERLIN_NAME, AllyCard.MERLIN_BATTLE_POINTS, new NoSpecial());
- assertEquals("Merlin", merlin.getName());
- assertEquals(0, merlin.getBattlePoints());
-
-
-
+		
+		// AMOUR
+		new AmourCard(new BidSpecial(AmourCard.AMOUR_BIDS));
+		
+		//fail("Not yet implemented");
 	}
 
 }
